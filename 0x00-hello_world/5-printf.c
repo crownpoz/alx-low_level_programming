a54b5e1352d184f05091a22baea41f68719822ff
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-08 06:04:09 +0000

    my fifth task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ * MY hello,printf programme
+ * Return:return 0 after programme execution
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return:(0);
+}
+
