6de9493839ab8da78eb10e60df0432a27aef9eea
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-09 08:51:05 +0000

    my 7th

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (success/correct)
  */
 
@@ -13,7 +13,7 @@ char alphabet;
 for (alphabet = 122 ; alphabet >= 97; alphabet--)
 {
 putchar(alphabet);
-}
+} 
 
 putchar('\n');
 
