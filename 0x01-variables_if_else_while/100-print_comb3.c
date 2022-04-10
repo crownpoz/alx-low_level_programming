5a5e9ba45d67ac54189c2b36e6553953462535b6
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-10 00:48:40 +0000

    my 10th

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * 
+ *
  * Return: Always 0
  */
 int main(void)
@@ -14,8 +14,8 @@ for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-putchar((digit1 %10) + '0');
-putchar((digit2 %10) + '0');
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
 if (digit1 == 8 && digit2 == 9)
 continue;
