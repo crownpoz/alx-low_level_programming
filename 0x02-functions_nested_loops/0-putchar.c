cfaeb0d67c3e26c758b7bd43c6daf4eec0785f30
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-13 03:52:14 +0000

    correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,18 @@
  *
  * Return: nothing, return void
  */
+ 
+ int main(void)
+ { 
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
 
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-Return (0);
-}
+ Return (0);
+ }
