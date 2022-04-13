c4d7e91cfd7e49795bf6657a3592ef55dce352a3
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-13 04:11:20 +0000

    1st task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_alphabet -print alphabet in lower case
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+char alphabet
+
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+Return (0);
+}
+
