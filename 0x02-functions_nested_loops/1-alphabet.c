42770ff190eccb45452aab720411f79d1ff0aee8
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-13 04:21:14 +0000

    really

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,9 @@
 
 void print_alphabet(void)
 {
-char alphabet
+char alphabet;
 
-for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
