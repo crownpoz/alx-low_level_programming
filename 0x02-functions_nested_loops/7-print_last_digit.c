9dfb04783466ca213664b5b731bfaeee8b5c27ad
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-13 06:22:33 +0000

    my seventh

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - last digit
- * 
+ *
  * @n: the int to print
  * Return: Always 0
  */
@@ -13,7 +13,7 @@ int print_last_digit(int n)
 int last_digit;
 
 if (n < 0)
-{ 
+{
 last_digit = (-1 * (n % 10));
 _putchar (last_digit + '0');
 return (last_digit);
