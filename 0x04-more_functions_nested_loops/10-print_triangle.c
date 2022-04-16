7be1faad40217cf94c6f0f670cbac72c4131b844
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-16 00:52:53 +0000

    tenth

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,6 +4,7 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
+
 void print_triangle(int size)
 {
 if (size <= 0)
@@ -13,14 +14,12 @@ _putchar('\n');
 else
 {
 int i, j;
-
 for (i =1; i <= size; i++)
 {
 for (j = i; j < size; j++)
 {
 _putchar(' ');
 }
-
 for (j = 1; j <= i; j++)
 {
 _putchar('#');
