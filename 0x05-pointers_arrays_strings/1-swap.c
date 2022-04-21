46c56b95f78db33f5972e16a3bee2594b00a8681
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-21 18:24:15 +0000

    1st commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers a and b
+ *@a: A ponter to an integer that will be updated
+ *@b: A pointer to an integer that will be updated
+ *Return: void that means our answer is correct
+ */
+
+void swap_int(int *a, int*b)
+{
+int f;
+
+f= *a;
+*a = *b;
+*b = f;
+}
