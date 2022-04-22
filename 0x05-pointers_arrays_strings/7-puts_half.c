440f9f726cae4c2b246de0037f1e5cf3c633ee8d
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-22 03:56:11 +0000

    7th task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *puts_half - prints every other character of a string
+ *@str: pointer to an int that would be changed
+ *
+ */
+ void puts_half(char *str)
+
+{
+int a; last;
+
+a = 0;
+while (str[a] != '\0')
+{
+a++;
+}
+
+last = (a + 1) / 2;
+
+for (a = last; str[a]; a++)
+{
+_putchar (str[a]);
+}
+
+_putchar ('\n'); 
+}
