70ad7e9194f81c88a872cc33eac3614033f722a9
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-25 04:29:34 +0000

    first task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,6 @@
  *@dest: string to be concatenated
  *@src: string to be concatenated until n limit
  *@n: number of bytes that will be used from src
- *
  *Return: pointer to the resulting string dest
  */
   
@@ -13,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 int c, i;
 
-for (c = 0; dest[c] != '\0'; c++)
+for (c = 0; dest[c] != '\0'; c++);
 
 for (i = 0; src[i] != '\0' && n > 0; i++, n--, c++)
 {
