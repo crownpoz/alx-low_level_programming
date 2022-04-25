6d7040adb1619bd077c5053a92a68697e87d2dd5
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-25 07:30:33 +0000

    second

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,9 +4,9 @@
  *_strncpy - function that copies a string
  *@dest: string to copy to
  *@src: string to be copied from
- *n: integer that determines number of bytes to be copied
+ *@n: integer that determines number of bytes to be copied
  *
- *Result: dest
+ *Return: pointer to the string dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -15,7 +15,7 @@ int x;
 
 for (x = 0; x < n && src[x] != '\0'; x++)
 dest[x] = src[x];
-for (;n > x; x++)
+for (; n > x; x++)
 dest[x] = '\0';
 
 return (dest);
