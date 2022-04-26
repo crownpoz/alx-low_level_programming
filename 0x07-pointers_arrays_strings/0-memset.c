bb7d1afdeda08027981c4f0afa6966af0c4f0a57
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-26 21:33:36 +0000

    0 task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,10 +2,10 @@
 
 /**
  *_memset - a function that fills memory with a constant byte
- *@s: input pointer to char type represents th pointer to the block of memory to fill
+ *@s: pointer to char type represents pointer to the block of memory to fill
  *@b: input variable of char type represents the character to fill s
  *@n: unsigned int variable the number of byte to be filled
-
+ *
  *Return: a pointer to the filled memory area @s
  */
 
