f9138068986d8a2385a376ed227d93c261e56f7b
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-27 06:08:15 +0000

    8th task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *set_string - sets the value of a pointer to char
+ *@s: content is to be copied
+ *@to: string
+ *return: void
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
