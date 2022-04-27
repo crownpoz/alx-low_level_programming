db194cbc980b819a4302d13e058da79539223c3a
crownpoz <agbeyoadebimpe@gmail.com>
2022-04-27 01:55:03 +0000

    1st task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,7 @@
 *Return: memory area replaced
 */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 unsigned int a;
 
 for (a = 0; a < n; a++)
