934f51a0c48ccad162b0e7a574cc60df00247329
crownpoz <agbeyoadebimpe@gmail.com>
2022-05-03 06:46:08 +0000

    0 task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0(sucess)
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
