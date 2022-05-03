b0d3ba5d4292363874fb050b1e0ea3da03025af2
crownpoz <agbeyoadebimpe@gmail.com>
2022-05-03 07:06:59 +0000

    2nd task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - prints all arguments it receives
+ *@argc: number of argumants
+ *@argv: array of arguments
+ *return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+int i;
+for(i = 0; i < argc ; argv++)
+printf("%s\n", argv[i]);
+return (0);
+}
