1cd676bbaa49276532c3791633c12efb9ac60bf3
crownpoz <agbeyoadebimpe@gmail.com>
2022-05-05 22:13:28 +0000

    0 task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,17 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    if (!size)
-       return (NULL);
-    
-    array = (char *) malloc(sizeof(char) * size);
-    if (array)
-    {
-       while (size > 0)
-                array[--size] = c;
-    }
-    return (array);
+	char *array;
+
+	if (!size)
+		return (NULL);
+
+	array = (char *) malloc(sizeof(char) * size);
+
+	if (array)
+	{
+		while (size > 0)
+			array[--size] = c;
+	}
+	return (array);
 }
