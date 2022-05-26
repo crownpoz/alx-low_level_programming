7dbbca46c7c3d6284812e6ffd228524ba8062cf6
crownpoz <agbeyoadebimpe@gmail.com>
2022-05-26 14:13:06 +0000

    first task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *listint_len - returns number of elements in a list
+ *@h: pointer to the next node
+ *Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t temp;
+
+	temp = h;
+	while (temp)
+	{
+		i++;
+		temp = temp->next;
+	}
+	return (i);
+}
