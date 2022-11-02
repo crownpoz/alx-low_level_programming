72b57d1877b2bfc51065419fbb01797f69f1cba6
crownpoz <agbeyoadebimpe@gmail.com>
2022-11-02 15:24:01 +0100

    fixed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,7 +6,6 @@
  * @array: is a pointer to the first element of the array to search in
  * @size: is the number of elements in array
  * @value: is the value to search for
- * 
  * Return: If value is not present in array or if array is NULL, return -1
  */
 
